c5b9600a9528d65ac6ba248d0fd8112afc5b7941
lriki <lriki.net@gmail.com>
2016-09-26 12:03:08 +0900

    test

---
+++ test/Test_CppLexer.cpp
@@ -0,0 +1,14 @@
+#include "TestConfig.h"
+
+class Test_Parser_CppLexer : public ::testing::Test
+{
+protected:
+	virtual void SetUp() {}
+	virtual void TearDown() {}
+};
+
+//------------------------------------------------------------------------------
+TEST_F(Test_Parser_CppLexer, Basic)
+{
+}
+
